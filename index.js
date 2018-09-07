a2db0b31cd897f5e48cbe0a033387c8dd79eaf8b
이진석 <sonaky47@gmail.com>
2018-09-07 23:57:40 +0900

    update

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
 
+app.set('port', 3000);
 app.use('/bower', express.static(__dirname + '/bower_components'));
 app.use('/node_modules/', express.static(__dirname + '/node_modules'));
 app.use('/', express.static(__dirname + '/src'));
-app.listen(8089, () => {
-  console.log('Server is running http://localhost:8089');
+app.listen(app.get('port'), () => {
+  console.log(`Server is running http://localhost:${app.get('port')}`);
 });
